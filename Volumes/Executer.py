27d2d060935127cec7afd56bffb5f903b153ebf7
Nikita Khabarov <khabarov.n.e@gmail.com>
2025-02-12 12:20:51 +0500

    Changings in exec

--- Volumes/Executer.py
+++ Volumes/Executer.py
@@ -1,17 +1,19 @@
-import seaborn as sns
-from Access.AccessInfo import AccessInfo as ai
 from Helpers.ParamsAndFuns import ParamsAndFuns as p
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from Helpers.DbConnector import DbConnector
 from Helpers.VolumesHelper import VolumesHelper
 p.set_np_pd_opts()
 
 
 source_path = r'D:\Khabarov\Репозиторий\sql_premises_and_volumes\SourceData\ИсходныеДанные.xlsx' #Здесь укажи путь к табл
-dbCon = DbConnector()
-dfFull = dbCon.get_volumes_df(source_path=source_path)
-volHel = VolumesHelper(dfFull)
+volHel = VolumesHelper(source_path=source_path)
+dfFull = volHel.fullDf
+
+#Выгрузка номенклатуры
+directory = r'D:\Khabarov\Репозиторий\sql_premises_and_volumes\Data'
+volHel.save_nomenclature(source_path=source_path,directory=directory)
+
+print(dfFull.isna().sum())
+
 
 
